1d00f2d3b27cd9574350b3ec662763ac93121c0a
Saida Davis <saihdavis@gmail.com>
2020-08-09 08:01:28 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,25 @@
+class Project
+  
+  attr_accessor :backer
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers
+    @backers
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+    
+  end
+    
+  
+  
+  
+  
+end
